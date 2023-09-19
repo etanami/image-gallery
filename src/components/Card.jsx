ed15c72b10a909f3dd3c08e0f56507093fdc3553
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-09-19 19:23:09 +0100

    feat image drag and drop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,63 @@
 /* eslint-disable react/prop-types */
+import { useRef } from 'react';
+import { useDrop, useDrag } from "react-dnd";
 
 // eslint-disable-next-line no-unused-vars
-const Card = ({ id, tag, src, index }) => {
+const Card = ({ id, tag, src, index, moveImage }) => {
+  const ref = useRef(null); 
+  
+  // define the drop area
+  const [, drop] = useDrop({
+    accept: "image",
+    hover: (item, monitor) => {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      
+      if (dragIndex === hoverIndex) {
+                  return;
+      }
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+          return;
+      }
+      
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+          return;
+      }
+      
+      moveImage(dragIndex, hoverIndex);
+      
+      item.index = hoverIndex;
+    }
+  });
+
+  // define the drag area
+  const [{ isDragging }, drag] = useDrag({
+    type: "image",
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor) => {
+      return {
+        isDragging: monitor.isDragging()
+      };
+    }
+  });
+        
+  // useDrag component  
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
+
   return (
-    <div className="my-2 p-2 text-center">
+    <div ref={ref} style={{ opacity }} className="my-2 p-2 text-center">
       <img src={src} alt={tag} width={400} height={400} className="object-cover h-full" />
       <p>{tag}</p>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Card from "../../components/Card";
 import imageList from "../../imagesData"
 
 const Home = () => {
   const [images, setImages] = useState(imageList);
+  
+  // move image from one location to another
+  const moveImage = useCallback((dragIndex, hoverIndex) => {
+    setImages((prevCards) => {
+    const clonedCards = [...prevCards];
+    const removedItem = clonedCards.splice(dragIndex, 1)[0];
+    clonedCards.splice(hoverIndex, 0, removedItem);
+    return clonedCards;
+  });
+  }, []);
 
   return (
     <div className="grid grid-cols-4 gap-4">
@@ -14,6 +24,7 @@ const Home = () => {
           src={image.thumb}
           id={image.id}
           index={index}
+          moveImage={moveImage}
         />
       ))}
     </div>
