340c49ca8939e2bf3314c96a8dc0b8e34cd35378
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-09-20 23:35:55 +0100

    add login and error page

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+
+const Login = () => {
+  return (
+    <div>
+      <form action="#">
+        <label htmlFor="">Username</label>
+        <input type="text" />
+        <label htmlFor="">Password</label>
+        <input type="password" />
+      </form>
+    </div>
+  )
+}
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+// import Home from './pages/Home/Home.jsx'
+import Login from './components/Login.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage /> 
+  },
+  {
+    path: 'login',
+    element: <Login />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
