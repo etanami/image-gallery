10fb43b19ceec11c63778345616ec9c4c3089c86
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-09-21 13:38:36 +0100

    fix images path

--- src/imagesData.js
+++ src/imagesData.js
@@ -3,41 +3,41 @@ export default [
   {
     id: 1,
     tag: 'Snake',
-    thumb: '../../../public/images/snake.jpg'
+    thumb: '/images/snake.jpg'
    },
    {
     id: 2,
     tag: 'Goat',
-    thumb: '../../../public/images/goat.jpg'
+    thumb: '/images/goat.jpg'
    },
    {
     id: 3,
     tag: 'Horse',
-    thumb: '../../../public/images/horse.jpg'
+    thumb: '/images/horse.jpg'
    },
    {
     id: 4,
     tag: 'Dog',
-    thumb: '../../../public/images/dog.jpg'
+    thumb: '/images/dog.jpg'
    },
    {
     id: 5,
     tag: 'Bird',
-    thumb: '../../../public/images/bird.jpg'
+    thumb: '/images/bird.jpg'
    },
    {
     id: 6,
     tag: 'Frog',
-    thumb: '../../../public/images/frog.jpg'
+    thumb: '/images/frog.jpg'
    },
    {
     id: 7,
     tag: 'Cat',
-    thumb: '../../../public/images/cat.jpg'
+    thumb: '/images/cat.jpg'
    },
    {
     id: 8,
     tag: 'Tiger',
-    thumb: '../../../public/images/tiger.jpg'
+    thumb: '/images/tiger.jpg'
    },
 ]
