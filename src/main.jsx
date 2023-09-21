159c35e115869e24155789117a6523679fd7f2f7
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-09-21 09:41:17 +0100

    remove login page and setup authentication

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 // import Home from './pages/Home/Home.jsx'
-import Login from './components/Login.jsx'
 import ErrorPage from './pages/ErrorPage.jsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 import Auth0Config from '../auth0-config.js'
@@ -15,17 +14,15 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage /> 
   },
-  {
-    path: 'login',
-    element: <Login />
-  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain={Auth0Config.domain}
     clientId={Auth0Config.clientId}
-    redirectUri={Auth0Config.redirectUri}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
   >
     <React.StrictMode>
       <RouterProvider router={router} />
